8d727a1dbf3763103016cf58df612fb6792ad127
zeroquinc <desiler@protonmail.com>
2024-04-08 01:36:26 +0200

    Rework of the Embed colors, more accurate to image

--- utils/image_utils.py
+++ utils/image_utils.py
@@ -3,6 +3,7 @@ import requests
 from PIL import Image
 from io import BytesIO
 from colorthief import ColorThief
+from collections import Counter
 
 """
 A function to get the most common color in an image. 
@@ -14,6 +15,25 @@ def get_most_common_color(image_url):
     img = Image.open(BytesIO(response.content))
     img.save("temp_image.png")  # save image temporarily
     color_thief = ColorThief("temp_image.png")
-    dominant_color = color_thief.get_color(quality=1)
+
+    # get color palette
+    palette = color_thief.get_palette(color_count=6)
+
+    # count pixels of each color
+    pixels = list(img.getdata())
+    color_counts = Counter(pixels)
+
+    # find most common and colorful color
+    most_colorful_color = None
+    max_saturation = -1
+    max_count = -1
+    for color in palette:
+        saturation = max(color) - min(color)  # calculate saturation
+        count = color_counts[color]
+        if saturation > 50 and count > max_count:  # exclude colors close to black, white, or grey
+            max_saturation = saturation
+            max_count = count
+            most_colorful_color = color
+
     os.remove("temp_image.png")  # delete the temporary image
-    return dominant_color[0] * 256 * 256 + dominant_color[1] * 256 + dominant_color[2]
+    return most_colorful_color[0] * 256 * 256 + most_colorful_color[1] * 256 + most_colorful_color[2]
