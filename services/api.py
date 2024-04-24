837213c82707c5ee52413f720bdbcba9dde25ad0
zeroquinc <desiler@protonmail.com>
2024-04-24 23:38:39 +0200

    Add better debugging to see when an API call is made

--- services/api.py
+++ services/api.py
@@ -2,6 +2,7 @@ import requests
 from typing import List, Optional
 
 from config.config import API_INTERVAL, BASE_URL
+from utils.custom_logger import logger
 
 from services.profile import Profile
 from services.game import Game
@@ -32,7 +33,9 @@ GameDetails: Get details about a game
 class GameDetails(BaseAPI):
     def __init__(self, game_id: str, api_username: str, api_key: str):
         super().__init__("API_GetGameExtended.php", {'z': api_username, 'y': api_key, 'i': game_id})
+        logger.debug(f"Fetching game details for game {game_id}")
         data = self.fetch_data()
+        logger.debug(f"API response: {data}")
         self.game = Game(data)
 
     def get_game(self) -> Game:
@@ -45,7 +48,9 @@ UserCompletionRecent: Get a user's recent achievements, defaults to 15 minutes
 class UserCompletionRecent(BaseAPI):
     def __init__(self, username: str, api_username: str, api_key: str):
         super().__init__("API_GetUserRecentAchievements.php", {'z': api_username, 'y': api_key, 'u': username, 'm': API_INTERVAL})
+        logger.debug(f"Fetching recent data for user {username}")
         data = self.fetch_data()
+        logger.debug(f"API response: {data}")
         self.user = username
         self.achievements = [Achievement(item) for item in data]
 
@@ -59,7 +64,9 @@ UserCompletionByDate: Get achievements earned by a user between two dates
 class UserCompletionByDate(BaseAPI):
     def __init__(self, username: str, api_username: str, api_key: str, start_date: str, end_date: str):
         super().__init__("API_GetAchievementsEarnedBetween.php", {'z': api_username, 'y': api_key, 'u': username, 'f': start_date, 't': end_date})
+        logger.debug(f"Fetching data for user {username} between {start_date} and {end_date}")
         data = self.fetch_data()
+        logger.debug(f"API response: {data}")
         self.user = username
         self.achievements = [Achievement(item) for item in data]
 
@@ -73,6 +80,7 @@ UserCompletionProgress: Get a user's completion progress, currently not used bec
 class UserCompletionProgress(BaseAPI):
     def __init__(self, username: str, api_username: str, api_key: str, count: int = 100, offset: int = 0):
         super().__init__("API_GetUserCompletionProgress.php", {'z': api_username, 'y': api_key, 'u': username, 'c': count, 'o': offset})
+        logger.debug(f"Fetching progress data for user {username}")
         data = self.fetch_data()
         self.user = username
         self.progress = Progress(data)
@@ -87,7 +95,9 @@ UserGameProgress: Get a user's progress in a game
 class UserProgressGameInfo(BaseAPI):
     def __init__(self, game_id: str, username: str, api_username: str, api_key: str):
         super().__init__("API_GetGameInfoAndUserProgress.php", {'z': api_username, 'y': api_key, 'u': username, 'g': game_id})
+        logger.debug(f"Fetching progress data for user {username} in game {game_id}")
         data = self.fetch_data()
+        logger.debug(f"API response: {data}")
         self.user = username
         self.game = Game(data)
 
@@ -101,7 +111,9 @@ UserProfile: Get a user's profile
 class UserProfile(BaseAPI):
     def __init__(self, username: str, api_username: str, api_key: str):
         super().__init__("API_GetUserProfile.php", {'z': api_username, 'y': api_key, 'u': username})
+        logger.debug(f"Fetching profile data for user {username}")
         data = self.fetch_data()
+        logger.debug(f"API response: {data}")
         self.profile = Profile(data)
 
     def get_profile(self) -> Profile:
