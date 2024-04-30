ddb191451422cf66c85df04f8d59fd9c9b28a8c4
zeroquinc <desiler@protonmail.com>
2024-04-30 17:05:28 +0200

    Refactor trophy retrieval logic if no games are played within the timeframe

--- trophies.py
+++ trophies.py
@@ -19,16 +19,18 @@ now = datetime.now(timezone.utc)
 titles = list(client.title_stats())
 title_ids = [title.title_id for title in titles if title.last_played_date_time > now - timedelta(days=4)]
 
-for trophy_title in client.trophy_titles_for_title(title_ids=title_ids):
-    # Print the icon URL of the trophy title
-    print("Trophy Title Icon URL:", trophy_title.title_icon_url)
+# Check if title_ids list is not empty
+if title_ids:
+    for trophy_title in client.trophy_titles_for_title(title_ids=title_ids):
+        # Print the icon URL of the trophy title
+        print("Trophy Title Icon URL:", trophy_title.title_icon_url)
 
-    # Get all trophies for the title
-    all_trophies = client.trophies(np_communication_id=trophy_title.np_communication_id, platform='PS4_game', trophy_group_id='all', include_metadata=True)
+        # Get all trophies for the title
+        earned_trophies = client.trophies(np_communication_id=trophy_title.np_communication_id, platform='all', trophy_group_id='all', include_metadata=True)
 
-    # Print the icon URLs of all earned trophies
-    for trophy in all_trophies:
-        if trophy.earned and trophy.earned_date_time > now - timedelta(days=4):
-            print("Achievement Name:", trophy.trophy_detail)
-            print("Trophy Type:", trophy.trophy_type.name.lower().capitalize())
-            print("Trophy Icon URL:", trophy.trophy_icon_url)
+        # Print the info of the earned trophies
+        for trophy in earned_trophies:
+            if trophy.earned and trophy.earned_date_time > now - timedelta(days=4):
+                print("Achievement Name:", trophy.trophy_detail)
+                print("Trophy Type:", trophy.trophy_type.name.lower().capitalize())
+                print("Trophy Icon URL:", trophy.trophy_icon_url)
