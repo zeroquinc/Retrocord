49c9506951d0bbe62beb4efa6cd6d8fe99193fb0
zeroquinc <desiler@protonmail.com>
2024-05-03 02:15:42 +0200

    chore: improve logging

--- utils/custom_logger.py
+++ utils/custom_logger.py
@@ -7,40 +7,38 @@ import sys
 LOG_LEVEL = 'DEBUG'
 discord_logger = None
 
-"""
-Creates and configures a logger with specified log level and formats for console and file logging.
-
-Args:
-    None
-
-Returns:
-    None
-
-Raises:
-    None
-"""
-
 def create_logger():
+    """
+    Creates and configures a logger with specified log level and formats for console and file logging.
+
+    Args:
+        None
+
+    Returns:
+        None
+
+    Raises:
+        None
+    """
     logs_path = Path('logs')
     logs_path.mkdir(exist_ok=True)
     log_format = "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level}</level> | <level>{message}</level>" if LOG_LEVEL != 'DEBUG' else "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level}</level> | <red>{file}</red> | <yellow>{function}</yellow> | <level>{message}</level>"
     logger.add(sys.stdout, level=LOG_LEVEL, colorize=True, format=log_format)
     logger.add(logs_path / 'retrocord.log', level=LOG_LEVEL, colorize=True, format=log_format)
 
-"""
-Switches the logger configuration based on the log level.
-
-Args:
-    None
-
-Returns:
-    None
-
-Raises:
-    ValueError: If the log level is invalid
-"""
-
 def switch_logger():
+    """
+    Switches the logger configuration based on the log level.
+
+    Args:
+        None
+
+    Returns:
+        None
+
+    Raises:
+        ValueError: If the log level is invalid
+    """
     logger.remove()
     if LOG_LEVEL in ['DEBUG', 'INFO']:
         create_logger()
@@ -48,10 +46,35 @@ def switch_logger():
         raise ValueError("Invalid log level")
 
 def log_json(json_obj, level='DEBUG'):
+    """
+    Logs a JSON object with the specified log level.
+
+    Args:
+        json_obj (dict): The JSON object to log.
+        level (str): The log level to use. Defaults to 'DEBUG'.
+
+    Returns:
+        None
+
+    Raises:
+        None
+    """
     pretty_json = json.dumps(json_obj, indent=4)
     logger.log(level, pretty_json)
 
 def setup_discord_logging():
+    """
+    Sets up logging for the 'discord' logger with a custom DiscordHandler.
+
+    Args:
+        None
+
+    Returns:
+        None
+
+    Raises:
+        None
+    """
     global discord_logger
     discord_logger = logging.getLogger('discord')
     discord_logger.setLevel(logging.DEBUG)
@@ -60,6 +83,18 @@ def setup_discord_logging():
     discord_logger.propagate = False
 
 class DiscordHandler(logging.Handler):
+    """
+    Custom logging handler for Discord, emits log records to the logger and Discord.
+
+    Args:
+        None
+
+    Returns:
+        None
+
+    Raises:
+        None
+    """
     def emit(self, record):
         log_entry = self.format(record)
         logger.log(record.levelno, log_entry)
